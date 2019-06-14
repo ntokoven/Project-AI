8bd6a4751aba15e5a96972835f9f70ac3e288d56
Nikita Tokovenko <ntokoven@gmail.com>
2019-06-14 21:09:27 +0200

    Update mine_2D_mov_avg.py
    Code ready for experiments with multiple MINE techniques (Regular, MINE-f and with Moving Average)

--- mine_2D_mov_avg.py
+++ mine_2D_mov_avg.py
@@ -9,10 +9,7 @@ import torch.optim as optim
 
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-
-def EMA(data, window_size=100):
-    return [np.mean(data[i:i+window_size]) for i in range(0,len(data)-window_size)]
-
+from collections import defaultdict
 
 
 class MINE(nn.Module):
@@ -30,13 +27,8 @@ class MINE(nn.Module):
 
     def forward(self, x, y):
         y_shuffled = y[torch.randperm(y.size()[0])]
-        #print(x.shape, '\n\n\n')
-        #print(y.shape, '\n\n\n')
-        #print(torch.cat((x, y), dim=1), '\n\n\n')
-        #print(self.T(torch.cat((x, y), dim=1)), '\n\n\n')
         T_joint = self.T(torch.cat((x, y), dim=1))
         T_marginal = self.T(torch.cat((x, y_shuffled), dim=1))
-
         return T_joint, T_marginal
 
     def lower_bound(self, x, y, method = 'kl', step = 2, ema = 0):
@@ -47,16 +39,56 @@ class MINE(nn.Module):
         elif method == 'f':
             mine = torch.mean(T_joint) - torch.mean(torch.exp(T_marginal - 1))   
         elif method == 'ema':
-            alpha = 1#2 / (batch_size + 1)
+            alpha = 2 / (batch_size + 1)
             if step == 1:
                 ema = torch.mean(torch.exp(T_marginal))
             else:
-                ema = (1 - alpha) * ema + alpha * torch.mean(torch.exp(T_marginal))  
-            #print(ema.mean(), ema)#.detach()) 
-            print(torch.mean(torch.exp(T_marginal)))
-            mine = torch.mean(T_joint) - (1 / ema.mean().detach()) * torch.mean(torch.exp(T_marginal))
+                ema = (1 - alpha) * ema + alpha * torch.mean(torch.exp(T_marginal)).detach()  
+            ema_normalization = (1 / ema) * torch.mean(torch.exp(T_marginal)).detach() #multiplying by detached mean to compensate the denominator after derivation of log
+            mine = torch.mean(T_joint) - ema_normalization * torch.log(torch.mean(torch.exp(T_marginal)))
             return -mine, ema 
         return -mine
+
+def train_MINE(data_loader, method, num_runs=1, epochs=200, lr=0.01):
+    for run in range(num_runs):
+        print('\n\nMethod - %s. Run - %s' % (method, run))
+        # Model and optimizer
+        model = MINE()#.cuda()
+        optimizer = optim.Adam(model.parameters(), lr=0.01)
+        
+        # Train
+        loss_list = []
+
+        model.train()
+        print('Run ', run)
+        ema = 1.
+        method = 'ema'
+        if num_runs > 1:
+            iterator = tqdm(range(epochs))
+        else: 
+            iterator = range(epochs)
+        for epoch in iterator:
+            loss_per_epoch = 0
+            step = 1
+            for _, (x, y) in enumerate(data_loader):
+                #x, y = x.cuda(), y.cuda()
+                model.zero_grad()
+                if method == 'ema':
+                    loss, ema = model.lower_bound(x, y, method, ema, step)
+                    step += 1
+                else:
+                    loss = model.lower_bound(x, y, method)
+                loss_per_epoch += loss
+                loss.backward()
+                optimizer.step()
+            loss_list.append(-loss_per_epoch / len(train_loader))  # since pytorch can only minimize the return of mine is negative, we have to invert that again
+            if epoch % 10 == 0:
+                print(epoch, -loss_per_epoch.detach().cpu().numpy() / len(train_loader))
+        
+        print('Real MI: ', MI)
+        print('Highest estimation: ', np.max(loss_list).item())
+        #print('Closest estimation: KL - %s, f - %s\n\n' % (best_result['kl'], best_result['f']))
+        return model, loss_list
 
 # Create data
 N = 10000
@@ -83,7 +115,7 @@ N_y = torch.distributions.multivariate_normal.MultivariateNormal(torch.zeros(2),
 N_x_y = torch.distributions.multivariate_normal.MultivariateNormal(torch.zeros(4), torch.from_numpy(cov))
 
 MI = N_x.entropy().numpy() + N_y.entropy().numpy() - N_x_y.entropy().numpy()
-print(MI)
+print('True value of Mutual Information - ', MI)
 
 batch_size = 1000
 # Dataloader
@@ -92,70 +124,22 @@ train = data_utils.TensorDataset(torch.from_numpy(data_x).float(),
                                 torch.from_numpy(data_y).float())
 train_loader = data_utils.DataLoader(train, batch_size=batch_size, shuffle=True, **kwargs) # becomes unstable and biased for batch_size of 100
 
-
-
-
-
-num_runs = 1
-winners = []
-for run in range(num_runs):
-    # Model and optimizer
-    model = MINE()#.cuda()
-    #model_f = MINE()
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
-    #optimizer_f = optim.Adam(model_f.parameters(), lr=0.01)
-    
-    # Train
-    loss_list = []
-    epochs = 200
-
-
-    model.train()
-    #model_f.train()
-    loss_track = {'kl':[], 'f':[], 'ema':[]}
-    print('Run ', run)
-    ema = 1.
-    method = 'kl'
-    for epoch in range(epochs):#tqdm(range(epochs)):
-        loss_per_epoch = 0
-        step = 1
-        for _, (x, y) in enumerate(train_loader):
-            #x, y = x.cuda(), y.cuda()
-            model.zero_grad()
-            #print('step', step)
-            if method == 'ema':
-                loss, ema = model.lower_bound(x, y, ema, step, method)
-            else:
-                loss = model.lower_bound(x, y, method)
-            loss_per_epoch += loss
-            loss.backward()
-            optimizer.step()
-            step += 1
-        #loss_list.append(-loss_per_epoch / len(train_loader))  # since pytorch can only minimize the return of mine is negative, we have to invert that again
-        if epoch % 10 == 0:
-            print(epoch, -loss_per_epoch.detach().cpu().numpy() / len(train_loader))
-        #loss_track['kl'].append(-loss_per_epoch.detach().cpu().numpy() / len(train_loader))
-        #loss_track['ema'].append(-loss_per_epoch.detach().cpu().numpy() / len(train_loader))
-        #loss_track['f'].append(-loss_per_epoch_f.detach().cpu().numpy() / len(train_loader))
-    #print(loss_track['kl'])
-    #print(np.max(loss_track['kl']))
-    #best_result = {'kl': np.abs(MI - np.max(loss_track['KL'])), 'f': np.abs(MI - np.max(loss_track['f']))}
-    #print('Real MI: ', MI)
-    #print('Highest estimations: KL - %s, f - %s' % (np.max(loss_track['kl']), np.max(loss_track['f'])))
-    #print('Closest estimations: KL - %s, f - %s\n\n' % (best_result['kl'], best_result['f']))
-    #winners.append(1 if best_result['kl'] < best_result['f'] else 0)
-
-#print('Winner: ', np.mean(winners))
+methods = ['kl', 'f', 'ema']
+epochs = 200
+loss_track = defaultdict(list)
+models = defaultdict()
+for method in methods:
+    models[method], loss_track[method] = train_MINE(train_loader, method, epochs=epochs)
 # Plot
 epochs = np.arange(1, epochs + 1)
 MI = np.repeat(MI, len(epochs))
-'''
+legend = ['MI']
 plt.figure(1)
 plt.plot(epochs, MI)
-plt.plot(epochs, loss_track['KL'])
-plt.plot(epochs, loss_track['f'])
-plt.legend(['MI', 'MINE_KL', 'MINE_f'])
+for method in methods:
+    plt.plot(epochs, loss_track[method])
+    legend.append('MI_%s' % method)
+plt.legend(legend)
 plt.ylabel('value')
 plt.xlabel('correlation')
 plt.show()
-#'''
