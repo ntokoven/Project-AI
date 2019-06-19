a1355fec4ca3aeb4765977f2a73730a31d9526a0
Nikita Tokovenko <ntokoven@gmail.com>
2019-06-19 17:03:53 +0200

    plot script

--- plot_pickle.py
+++ plot_pickle.py
@@ -1,15 +1,26 @@
 import pickle
 import argparse
+import numpy as np
+import matplotlib.pyplot as plt
 
 def main():
     parser = argparse.ArgumentParser(description='read pickle')
-    parser.add_argument('--import_path', type=str, default="mi_values_dict", metavar='N',
+    parser.add_argument('--import-path', type=str, default="mi_values_dict", metavar='N',
                         help='none')
     args = parser.parse_args()
     pickle_in = open(args.import_path+".pickle","rb")
     example_dict = pickle.load(pickle_in)
+    epochs = np.arange(1, 50 + 1)
+    plt.figure(1)
     for i in example_dict.keys():
-        print(i,example_dict[i])
+        #print(i, example_dict[i])
+        plt.plot(example_dict[i], epochs, label=i)
+    plt.legend()
+    plt.ylabel('epochs')
+    plt.xlabel('MINE_values')
+    plt.show()
+    plt.savefig('result.png')
+
 
 if __name__ == '__main__':
     # freeze_support() here if program needs to be frozen
