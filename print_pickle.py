72e54d1bb14f907a58036fe1cec6a0d924e26a5e
Nikita Tokovenko <ntokoven@gmail.com>
2019-06-20 19:18:51 +0200

    Update everything

--- print_pickle.py
+++ print_pickle.py
@@ -2,12 +2,14 @@ import pickle
 import argparse
 def main():
     parser = argparse.ArgumentParser(description='read pickle')
-    parser.add_argument('--import_path', type=str, default="mi_values_dict", metavar='N',
+    parser.add_argument('--import-path', type=str, default="mi_values_dict", metavar='N',
                         help='none')
     args = parser.parse_args()
     pickle_in = open(args.import_path+".pickle","rb")
     example_dict = pickle.load(pickle_in)
     for i in example_dict.keys():
+        #for j in range(len(example_dict[i])):
+        #    print(example_dict[i][j].item())# = example_dict[i][j].
         print(i,example_dict[i])
 
 if __name__ == '__main__':
