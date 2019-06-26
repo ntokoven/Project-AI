f9633cafdd3a9903749f42d80dc153d452f8d241
Nikita Tokovenko <ntokoven@gmail.com>
2019-06-26 21:48:02 +0200

    updated plots

--- plot_pickle_compare_ft_init.py
+++ plot_pickle_compare_ft_init.py
@@ -7,24 +7,26 @@ def main():
     parser = argparse.ArgumentParser(description='read pickle')
     parser.add_argument('--import-path', type=str, default="mi_values_dict", metavar='N',
                         help='none')
-    parser.add_argument('--title', type=str, default="", metavar='N',
-                        help='none')
     args = parser.parse_args()
     pickle_in = open(args.import_path+".pickle","rb")
     mi_values = pickle.load(pickle_in)
-    epochs = np.arange(1, 30 + 1)
+    epochs = np.arange(1, 2 + 1)
     values = {}
-    values = np.array(mi_values['relu3T'])
-    print(values.shape)
+    for key in mi_values.keys():
+        values[key] = np.array(mi_values[key])#.reshape(180)
+        print(values[key].shape)
     plt.figure(1)
-    for epoch in range(len(values)):
-        plt.plot(epochs, values[epoch][:30], label='Run %s' % (epoch + 1))
+    for key in mi_values.keys():
+        print(len(mi_values[key]), len(mi_values[key][1]))
+    for i in mi_values.keys():
+        #print(i, example_dict[i])
+        plt.plot(epochs, np.max(values[i], axis=1), label=i)
     plt.legend()
-    plt.ylabel('MI(Y, relu3)')
+    plt.ylabel('MI(x, L_i + eps)')
     plt.xlabel('MINE epochs')
-    plt.savefig('%s+%s_T.png' % (args.import_path, args.title))
+    plt.savefig('%s.png' % args.import_path)
     plt.show()
-   
+
 
 if __name__ == '__main__':
     # freeze_support() here if program needs to be frozen
